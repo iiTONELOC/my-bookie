65898eb49b0af7e2166b45019a62579b7c086492
Anthony T <anthonytropeano@me.com>
2022-01-31 23:00:26 -0500

    dunno how to configure the wsgi for heroku, see if this does it

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 from flask_server.main import my_app
 from dotenv import load_dotenv
 load_dotenv()
-def app(): return my_app.run()
+def app(): return my_app
 
 
+my_app = app()
 if __name__ == '__main__':
-    app()
+    app().run()
